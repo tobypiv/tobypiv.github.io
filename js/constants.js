72ae1f7e3cc34ac0a07c68ba24402bd175f9de06
tobypiv <88910996+tobypiv@users.noreply.github.com>
2022-11-02 17:20:11 +1100

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -10,11 +10,11 @@ let constants = {
         nt: { name: "nt", seats: 2, displayName: "Northern Teritory" },
     },
     parties: {
-        labor: { name: "labor", getColour: (a) => `rgba(222, 43, 52, ${a})` },
-        liberal: { name: "liberal", getColour: (a) => `rgba(28, 79, 156, ${a})` },
-        greens: { name: "greens", getColour: (a) => `rgba(0, 156, 61, ${a})` },
-        national: { name: "national", getColour: (a) => `rgba(0, 105, 70, ${a})` },
-        "united australia": { name: "united australia", getColour: (a) => `rgba(254, 237, 1, ${a})` },
+        labor: { name: "labor", getColour: (a) => `rgba(222, 43, 52, ${a})`, preferences: ["greens", "liberal", "national", "united australia"] },
+        liberal: { name: "liberal", getColour: (a) => `rgba(28, 79, 156, ${a})`, preferences: ["national", "labor", "greens", "united australia"] },
+        greens: { name: "greens", getColour: (a) => `rgba(0, 156, 61, ${a})`, preferences: ["labor", "liberal", "national", "united australia"] },
+        national: { name: "national", getColour: (a) => `rgba(0, 105, 70, ${a})`, preferences: ["liberal", "united australia", "labor", "greens"] },
+        "united australia": { name: "united australia", getColour: (a) => `rgba(254, 237, 1, ${a})`, preferences: ["liberal", "national", "labor", "greens"] },
     },
     levels: ["2022"],
 };
